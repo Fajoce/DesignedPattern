259822f3e8e52bb31cc8f0bb35f46663046ee3b6
fajoce <fajoce@gmail.com>
2025-06-10 16:39:01 -0500

    I added a validation on updateProductDTO

--- PaternDesign.API/Domain/DTOs/UpdateProductDTO.cs
+++ PaternDesign.API/Domain/DTOs/UpdateProductDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace PaternDesign.API.Domain.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaternDesign.API.Domain.DTOs
 {
     public class UpdateProductDTO
     {
         public int ProductId { get; set; }
+        [Required]
         public string ProductName { get; set; } = string.Empty;
         public string ProductDescription { get; set; } = string.Empty;
         public decimal ProductPrice { get; set; } = 0;
