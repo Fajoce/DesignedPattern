6a032535a1c5a30e92ef2854a8a12bc41ac4e072
fajoce <fajoce@gmail.com>
2025-06-10 15:32:03 -0500

    Changes

--- PaternDesign.API/Domain/DTOs/UpdateProductDTO.cs
+++ PaternDesign.API/Domain/DTOs/UpdateProductDTO.cs
@@ -3,8 +3,8 @@
     public class UpdateProductDTO
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
         public decimal ProductPrice { get; set; } = 0;
     }
 }
